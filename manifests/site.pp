58110a2021629fa0bf19b5292f1d60568ff4b771
Rolf Erik Lekang <me@rolflekang.com>
2013-11-06 12:40:49 +0100

    add site manifest

---
+++ manifests/site.pp
@@ -0,0 +1,23 @@
+# == Define jekyll:site
+
+define jekyll::site (
+  $domain,
+  $repo,
+  $ensure = 'present',
+  $vcs_type = 'git',
+  $site_path = "${base_path}/${title}"
+) {
+  include jekyll::ngnix
+
+  vcsrepo { $site_path:
+    ensure   => $ensure,
+    provider => $vcs_type,
+    source   => $repo,
+  }
+
+  exec { "build-${title}":
+    command => 'jekyll build',
+    cwd     => $site_path,
+    require => Vcsrepo[$title]
+  }
+}
